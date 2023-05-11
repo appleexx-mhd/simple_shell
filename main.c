49b603e015c32810bca45baf6413bae0df0b3b03
appleexx-mhd <mr.mhdhilali@email.com>
2023-05-11 11:49:25 +0100

    Update

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 int main(int ac, char **argv)
 {
     char *prompt = "(Shell) $";
-    char *lineptr;
+    char *line;
     size_t b = 0;
+    ssize_t read_char;
 
     // declaring void variables
     (void)ac;
@@ -12,8 +13,17 @@ int main(int ac, char **argv)
     // infinte loop
     while (1) {
         printf("%s", prompt);
-        getline(&lineptr, &b, stdin);
-        printf("%s\n", lineptr);
+        read_char = getline(&line, &b, stdin);
+
+        //check if getline function failed or reashed EOF
+        if (read_char == -1)
+        {
+            printf("Exiting Shell......\n");
+            return (-1);
+        }
+        
+        printf("%s\n", line);
+
         // free the allocated memory after every execution
         free(lineptr);
     }
